c50d59707e78fdbe910aa93e4b4d8f7d64b458e5
Carlos Herrera <29789062+Carlos4ndresh@users.noreply.github.com>
2023-09-04 18:23:11 -0300

    Added new org for only accounts used for certification

--- global/organizations/main.tf
+++ global/organizations/main.tf
@@ -10,3 +10,8 @@ resource "aws_organizations_organizational_unit" "dev" {
   name      = "dev"
   parent_id = aws_organizations_organization.cahp_main_org.roots[0].id
 }
+
+resource "aws_organizations_organizational_unit" "cert" {
+  name      = "cert"
+  parent_id = aws_organizations_organization.cahp_main_org.roots[0].id
+}
