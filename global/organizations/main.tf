55ae4d4c8a103aacf87b1701ac625e320a3e1877
Carlos Herrera <29789062+Carlos4ndresh@users.noreply.github.com>
2023-10-12 20:49:16 -0300

    testing adding an ou

--- global/organizations/main.tf
+++ global/organizations/main.tf
@@ -11,6 +11,12 @@ resource "aws_organizations_organizational_unit" "dev" {
   parent_id = aws_organizations_organization.cahp_main_org.roots[0].id
 }
 
+resource "aws_organizations_organizational_unit" "prod" {
+  name      = "prod"
+  parent_id = aws_organizations_organization.cahp_main_org.roots[0].id
+}
+
+
 resource "aws_organizations_organizational_unit" "cert" {
   name      = "cert"
   parent_id = aws_organizations_organization.cahp_main_org.roots[0].id
