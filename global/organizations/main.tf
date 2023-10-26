5e371dfd691e6eaafc8019f2a1bd55e68da97813
Carlos Herrera <29789062+Carlos4ndresh@users.noreply.github.com>
2023-10-26 20:43:47 -0300

    Created AFT OU

--- global/organizations/main.tf
+++ global/organizations/main.tf
@@ -2,6 +2,12 @@ resource "aws_organizations_organization" "cahp_main_org" {
   aws_service_access_principals = [
     "member.org.stacksets.cloudformation.amazonaws.com",
     "ssm.amazonaws.com",
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com",
+    "controltower.amazonaws.com",
+    "sso.amazonaws.com",
   ]
-
+  enabled_policy_types = [
+    "SERVICE_CONTROL_POLICY",
+  ]
 }
