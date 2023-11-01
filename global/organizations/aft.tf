a5679aa8b3c292a48ec345bbc0ba1f2880bf7fc1
Carlos Herrera <29789062+Carlos4ndresh@users.noreply.github.com>
2023-11-01 10:53:31 -0300

    Added module for aft

---
+++ global/organizations/aft.tf
@@ -0,0 +1,15 @@
+module "aft" {
+  source                      = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  ct_management_account_id    = "982656938909"
+  log_archive_account_id      = "249670792191"
+  audit_account_id            = "084565650240"
+  aft_management_account_id   = "307701292113"
+  ct_home_region              = "us-west-2"
+  tf_backend_secondary_region = "us-west-2"
+  # VCS Vars
+  vcs_provider                                  = "github"
+  account_request_repo_name                     = "Carlos4ndresh/aws_account_requests"
+  global_customizations_repo_name               = "ExampleOrg/example-repo-2"
+  account_customizations_repo_name              = "ExampleOrg/example-repo-3"
+  account_provisioning_customizations_repo_name = "ExampleOrg/example-repo-4"
+}
